44275e583b4f99235235f7e4355e3817e943bf9b
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-01-06 16:11:11 +0900

    Rename simplemd to simplenotes

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
 #include "MainWindow.h"
 #include "NotesListView.h"
 
+#include <QStandardPaths>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     
     MainWindow mainWindow;
+
+    qDebug() << QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+
 
     auto* model = new ProjectFolderModel{};
     
