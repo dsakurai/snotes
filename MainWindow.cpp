3b1ca8e874d99a0de4c5e7c748871838ea2c9f55
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-01-08 18:38:26 +0900

    Create a new note.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -8,6 +8,7 @@
 #include "ui_MainWindow.h"
 #include "Settings.h"
 
+#include <QShortcut>
 
 MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent), ui(new Ui::MainWindow) {
@@ -20,6 +21,40 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(
             ui->notesListView, &NotesListView::singleFileSelected,
             ui->markdownEditor, &SNotesMarkdownEditor::open_file
+    );
+
+    // Create a new note
+    QShortcut *shortcut = new QShortcut(QKeySequence::New, this);
+    connect(shortcut, &QShortcut::activated,
+            [this]() {
+                QString dir_path = ui->notesListView->model()->rootPath();
+                QDir dir (dir_path);
+                QString baseName = dir.path() + QDir::separator() + "Untitled";
+                QString extension = ".md";
+                int counter = 1;
+
+                QString file_path = baseName + QString::number(counter) + extension;
+                QFile file(file_path);
+
+                while (file.exists()) {
+                    counter++;
+                    file_path = baseName + QString::number(counter) + extension;
+                    file.setFileName(file_path);
+                }
+
+                if (!file.open(QIODevice::WriteOnly)) {
+                    /* Display warning */ QMessageBox::critical(this, "File Creation Fail", "Creating a new file failed. Check the folder permission.");
+                    return;
+                }
+                file.close();
+                
+                // Select file in list view now that the file is created.
+                auto index = ui->notesListView->model()->index(file_path);               
+                if (!index.isValid()) return;
+                
+                ui->notesListView->selectionModel()->select(index, QItemSelectionModel::Select | QItemSelectionModel::Rows);
+                ui->notesListView->scrollTo(index);
+            }
     );
 }
 
