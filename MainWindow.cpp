be5d35ce7e69906b9505f6b254939465c4214d1a
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-02-10 21:58:30 +0900

    Create a new file with a shortcut.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -80,38 +80,38 @@ MainWindow::MainWindow(QWidget *parent) :
     );
 
     // Create a new note
-//    QShortcut *shortcut = new QShortcut(QKeySequence::New, this);
-//    connect(shortcut, &QShortcut::activated,
-//            [this]() {
-//                QString dir_path = ui->notesListView->model()->rootPath();
-//                QDir dir (dir_path);
-//                QString baseName = dir.path() + QDir::separator() + "Untitled";
-//                QString extension = ".md";
-//                int counter = 1;
-//
-//                QString file_path = baseName + QString::number(counter) + extension;
-//                QFile file(file_path);
-//
-//                while (file.exists()) {
-//                    counter++;
-//                    file_path = baseName + QString::number(counter) + extension;
-//                    file.setFileName(file_path);
-//                }
-//
-//                if (!file.open(QIODevice::WriteOnly)) {
-//                    /* Display warning */ QMessageBox::critical(this, "File Creation Fail", "Creating a new file failed. Check the folder permission.");
-//                    return;
-//                }
-//                file.close();
-//                
-//                // Select file in list view now that the file is created.
-//                auto index = ui->notesListView->model()->index(file_path);               
-//                if (!index.isValid()) return;
-//                
-//                ui->notesListView->selectionModel()->select(index, QItemSelectionModel::Select | QItemSelectionModel::Rows);
-//                ui->notesListView->scrollTo(index);
-//            }
-//    );
+    QShortcut *shortcut = new QShortcut(QKeySequence::New, this,
+                                        [this]() {
+                                            QString dir_path = ui->notesListView->model()->rootPath();
+                                            QDir dir (dir_path);
+                                            QString baseName = dir.path() + QDir::separator() + "Untitled";
+                                            QString extension = ".md";
+                                            int counter = 1;
+
+                                            QString file_path = baseName + QString::number(counter) + extension;
+                                            QFile file(file_path);
+
+                                            while (file.exists()) {
+                                                counter++;
+                                                file_path = baseName + QString::number(counter) + extension;
+                                                file.setFileName(file_path);
+                                            }
+
+                                            if (!file.open(QIODevice::WriteOnly)) {
+                                                /* Display warning */ QMessageBox::critical(this, "File Creation Fail", "Creating a new file failed. Check the folder permission.");
+                                                return;
+                                            }
+                                            file.close();
+
+                                            // Select file in list view now that the file is created.
+                                            auto index = ui->notesListView->model()->index(file_path);
+                                            if (!index.isValid()) return;
+
+                                            ui->notesListView->selectionModel()->select(index, QItemSelectionModel::Select | QItemSelectionModel::Rows);
+                                            ui->notesListView->scrollTo(index);
+                                        },
+                                        Qt::ShortcutContext::WindowShortcut
+    );
 
     connect(
             ui->notesListView, &NotesListView::singleFileSelected,
