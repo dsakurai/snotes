fdf09f90382223856f9fb36c8137df6b43e18323
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-02-10 20:13:38 +0900

    Move IO functions into IO class

--- SNotesMarkdownEditor.h
+++ SNotesMarkdownEditor.h
@@ -16,100 +16,106 @@ public:
 
     class IO {
     public:
-        IO(const QPointer<SNotesMarkdownEditor>& editor): editor {editor}
-         {}
+        IO(const QPointer<SNotesMarkdownEditor>& editor, QString path):
+                editor {editor},
+                path {path}
+         {
+             readAllNow();
+
+             QObject::connect(
+                     editor.get(), &SNotesMarkdownEditor::textChanged,
+                     [this]() { request_save(); }
+             );
+
+             auto* ask_to_save_timer = new QTimer(editor);
+             QObject::connect(
+                     ask_to_save_timer, &QTimer::timeout,
+                     [this]() {
+                         if (is_save_requested) save_file_immediately();
+                     }
+             );
+             
+             QObject::connect(
+                editor.get(), &SNotesMarkdownEditor::textChanged,
+                [this, ask_to_save_timer=ask_to_save_timer]() {
+                    ask_to_save_timer->start(1000); // every second
+                }
+             );
+         }
          
-         QString readAllNow() {
+         void readAllNow() {
 
-             if (editor.isNull()) return {};
+             if (editor.isNull()) return;
 
-             auto path = editor->getPath();
-             if (!path) return {};
+             if (!path) return;
              
              if (QFile file {*path};
                      file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-                 return file.readAll();
+
+                 editor->setPlainText(
+                         file.readAll());
+
              } else {
                  /* Display warning */ QMessageBox::critical(nullptr, "Reading Fail", "Reading from the file failed. Check the file permissions.");
-                 return {};
+                 return;
              }
          }
          
-         QPointer<SNotesMarkdownEditor> editor;
+        inline
+        void save_file_immediately() {
+        
+            qDebug() << "saving file";
+
+            is_save_requested = false;
+            
+            if (editor.isNull()) return;
+            
+            if (!path) return; // No path set.
+
+            QString original; // Original text in the file
+            if (QFile file (*path); file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                original = file.readAll();
+            } else {
+                // If we can't read from the file, we don't know if the content in the editor matches with the actual file contents we failed to read.
+                // We therefore cancel the write and quit.
+                /* Display warning */ QMessageBox::critical(nullptr, "Reading Fail", "Reading from the file failed. Check the file permissions.");
+                return;
+            }
+
+            // The text in the editor
+            const QString text = editor->document()->toPlainText(); // Don't use toRawText(). It will use the wrong line separator (maybe \r, I don't know which).
+            if (text == original) return; // no point writing the same text
+
+            // Write the content to file
+            if (QFile file (*path); file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate )) {
+                file.write(text.toUtf8());
+            } else /* Display warning */ QMessageBox::critical(nullptr, "Writing Fail", "Writing to the file failed. Check the file permissions.");
+        }
+
+        void request_save() {is_save_requested = true;}
+        
+        ~IO() {
+            if (path) save_file_immediately();
+        }
+        
+    private:
+        QPointer<SNotesMarkdownEditor> editor;
+        std::optional<QString> path;
+        bool is_save_requested = false;
     };
     std::unique_ptr<IO> io;
 
     explicit
     SNotesMarkdownEditor(QString path, QWidget* parent = nullptr):
-            QMarkdownTextEdit(parent),
-            io {std::make_unique<IO>(this)},
-            path {path}
+            QMarkdownTextEdit(parent)
     {
-        
-        setPlainText(
-                io->readAllNow()
-        );
-        
-        connect(
-            this, &SNotesMarkdownEditor::textChanged,
-            this, &SNotesMarkdownEditor::request_save
-        );
-        
-        auto* ask_to_save_timer = new QTimer(this);
-        connect(
-                ask_to_save_timer, &QTimer::timeout,
-                [this]() {
-                    if (is_save_requested) save_file_immediately();
-                }
-        );
-        ask_to_save_timer->start(1000); // every second
-    }
-
-    ~SNotesMarkdownEditor() override {
-        if (path) save_file_immediately();
+        // Do this inside the constructor (not in the initializer list) because IO might call methods of SNotesMarkdownEditor.
+        io = std::make_unique<IO>(this, path);
     }
     
-public slots:
-    
-    inline
-    void save_file_immediately() {
-    
-        is_save_requested = false;
-    
-        if (!path) return; // No path set.
-        
-        QString original; // Original text in the file
-        if (QFile file (*path); file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            original = file.readAll();
-        } else {
-            // If we can't read from the file, we don't know if the content in the editor matches with the actual file contents we failed to read.
-            // We therefore cancel the write and quit.
-            /* Display warning */ QMessageBox::critical(this, "Reading Fail", "Reading from the file failed. Check the file permissions.");
-            return;
-        }
-
-        // The text in the editor
-        const QString text = this->document()->toPlainText(); // Don't use toRawText(). It will use the wrong line separator (maybe \r, I don't know which).
-        if (text == original) return; // no point writing the same text
-        
-        // Write the content to file
-        if (QFile file (*path); file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate )) {
-            file.write(text.toUtf8());
-        } else /* Display warning */ QMessageBox::critical(this, "Writing Fail", "Writing to the file failed. Check the file permissions.");
-    }
-
-protected slots:
-    void request_save() { is_save_requested = true; };
-
-
 public:
-    const std::optional<QString> &getPath() const {
-        return path;
-    }
     
 private:
-    bool is_save_requested = false;
-    std::optional<QString> path;
 };
 
 
